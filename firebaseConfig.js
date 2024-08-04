1e5929a7a24a8b332ac15975bf5c929a8940379b
KirtimanNanda <173339421+EduLips@users.noreply.github.com>
2024-08-04 13:11:01 +0530

    ENV

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,22 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./config/firebaseConfig.js');
 
-// Initialize Firebase Admin SDK
-if (!admin.apps.length) {
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-        databaseURL: 'https://edushort-cce1c-default-rtdb.firebaseio.com/'
-    });
-}
+const serviceAccount = {
+    type: 'service_account',
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: 'firebase-adminsdk-c3u1f@edushort-cce1c.iam.gserviceaccount.com',
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+};
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://edushort-cce1c-default-rtdb.firebaseio.com/' // Add your database URL here
+});
 
 const db = admin.database();
 module.exports = db;
